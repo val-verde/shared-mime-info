363e903f7400037648eaa6f79a22e09815a79bb8
Shawn Rutledge <shawn.rutledge@qt.io>
2018-01-26 08:34:08 +0100

    Change Qt Linguist file type to text/vnd.qt.linguist
    TrollTech doesn't exist anymore: now Qt is the name of the company,
    the non-profit foundation, and the product. This change will makes this
    file type more recognizable in the future when TrollTech may be
    forgotten.
    
    Also update linguist.ts to look like a current (Qt 5.11) translation file.
    
    https://bugs.freedesktop.org/show_bug.cgi?id=104799

--- tests/linguist.ts
+++ tests/linguist.ts
@@ -1,9 +1,11 @@
-<!DOCTYPE TS><TS>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
 <context>
     <name>QPushButton</name>
     <message>
-        <source>Hello world!</source>
-        <translation type="unfinished"></translation>
+        <source>Hello World!</source>
+        <translation>Hallo Welt!</translation>
     </message>
 </context>
 </TS>

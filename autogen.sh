b61f41168ba767f475e12763c581cf86bd48391b
Bastien Nocera <hadess@hadess.net>
2018-10-13 14:57:45 +0200

    build: Fix autogen.sh for srcdir != builddir

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+pushd $srcdir
 echo "autogen.sh: running: intltoolize --force"
 intltoolize --force || exit 1
 autoreconf -vif || exit 1
 
+popd
+
 if test -z "$NOCONFIGURE"; then
-    ./configure --enable-maintainer-mode "$@" && echo "Now type \`make' to compile" || exit 1
+    $srcdir/configure --enable-maintainer-mode "$@" && echo "Now type \`make' to compile" || exit 1
 fi

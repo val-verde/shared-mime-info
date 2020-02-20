1ed2557d5eb5dfe8dfbd15ea705e9f480b446e29
Bastien Nocera <hadess@hadess.net>
2020-02-20 18:20:05 +0100

    build: Remove autotools support from freedesktop_generate.sh

--- data/freedesktop_generate.sh
+++ data/freedesktop_generate.sh
@@ -1,24 +1,12 @@
 #!/bin/bash -e
 
-if [ "$1" == "--meson" ] ; then
-	shift
-	MESON=enabled
-fi
 src_root="$1"
 build_root="$2"
 
-if test "x$MESON" = "x" ; then
-	make -C "${build_root}/po" update-gmo
-	OUT="${build_root}/freedesktop.org.xml"
-	GMODIR="${src_root}/po/"
-else
-	ninja -C "${build_root}" shared-mime-info-gmo
-	OUT="${build_root}/data/freedesktop.org.xml"
-	GMODIR="${build_root}/po/"
-fi
+ninja -C "${build_root}" shared-mime-info-gmo
 
 itstool \
     --its "${src_root}/data/its/shared-mime-info.its" \
     --join "${src_root}/data/freedesktop.org.xml.in" \
-    -o "${OUT}" \
-    "${GMODIR}"*".gmo"
+    -o "${build_root}/data/freedesktop.org.xml" \
+    "${build_root}/po/"*".gmo"

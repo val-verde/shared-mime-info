5a5a3f7dd426679417c35b07925a9e916972be99
Bastien Nocera <hadess@hadess.net>
2020-03-02 17:38:46 +0100

    po: Add script to pull translations from Transifex

---
+++ po/update_translations.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+if [ ! -f po/shared-mime-info.pot ] ; then
+	echo "Please generate “po/shared-mime-info.pot” before running this command"
+	exit 1
+fi
+
+if [ ! -f .tx/config ] ; then
+	echo "Please run this command from the root of the source directory"
+	exit 1
+fi
+
+tx pull --all --force --skip
+tx push --source

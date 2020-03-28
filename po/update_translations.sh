9030c21cf7e9ad6fc5dcd6a7666665049ad4159e
Bastien Nocera <hadess@hadess.net>
2020-03-28 12:27:06 +0100

    po: Add check to translations update script

--- po/update_translations.sh
+++ po/update_translations.sh
@@ -7,6 +7,11 @@ if [ ! -f po/shared-mime-info.pot ] ; then
 	exit 1
 fi
 
+if [ data/freedesktop.org.xml.in -nt po/shared-mime-info.pot ] ; then
+	echo "Please update “po/shared-mime-info.pot” before running this command"
+	exit 1
+fi
+
 if [ ! -f .tx/config ] ; then
 	echo "Please run this command from the root of the source directory"
 	exit 1

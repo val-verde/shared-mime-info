aaeb7a135a062c2fd85ed004e345cac7c267c999
Ville Skyttä <ville.skytta@iki.fi>
2020-12-26 10:35:23 +0200

    tests: implement duplicate type check with xpath, check aliases too

--- tests/test_duplicate_mime_types.sh
+++ tests/test_duplicate_mime_types.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
+: ${1:?filename argument missing}
 xml_db_file="${1}"
+test -f ${xml_db_file} || {
+    printf "%s: no such file\n" ${xml_db_file} >&2
+    exit 1
+}
 
 duplicated=$(
-    grep '<mime-type' ${xml_db_file} | tr -d '[:blank:]' | sort | uniq -d
+    xmllint --xpath \
+            "//*[local-name()='mime-type' or local-name()='alias']/@type" \
+            ${xml_db_file} | tr ' ' '\n' | sort | uniq -d
 )
 
 if [[ -n "${duplicated}" ]]; then

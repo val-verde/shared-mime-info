136cb8f1d3bedc79343cad51ed744d69f5508adf
orbea <orbea@riseup.net>
2021-10-01 14:57:47 -0700

    freedesktop_generate.sh: Don't hardcode ninja
    Another ninja implementation that works is samurai
    
    https://github.com/michaelforney/samurai

--- data/freedesktop_generate.sh
+++ data/freedesktop_generate.sh
@@ -3,7 +3,7 @@
 src_root="$1"
 build_root="$2"
 
-ninja -C "${build_root}" shared-mime-info-gmo
+${NINJA:-ninja} -C "${build_root}" shared-mime-info-gmo
 
 itstool \
     --its "${src_root}/data/its/shared-mime-info.its" \
